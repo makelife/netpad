af2029a807d501cbc21d1bdeaced8abd88b102cb
James Telfer <jameswtelfer@bigpond.com>
2011-03-10 20:20:06 +1100

    JWT

---
+++ Assets/ntpdClientConnection.cs
@@ -0,0 +1,18 @@
+// Netpad [ntpd]
+// Netpad is released under the Artistic Licence 2.0. The full licence can be found in the root directory.
+// Contributing authors: James Telfer [jt]
+
+// ntpdClientConnection
+// The networking interface initilisation class.
+// Handles display of servers available, joining servers, starting a new one and disconnecting.
+
+using UnityEngine;
+using System.Collections;
+
+public class ntpdClientConnection : MonoBehaviour {
+
+	void Start () 
+	{
+	
+	}
+}
